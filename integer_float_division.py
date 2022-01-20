30a0d2c86af9693708f8d3538342cf231dd01801
Rishab Edmund Edmund <98113375+rmeajuri@users.noreply.github.com>
2022-01-20 17:11:11 +0000

    Created a float division and integer division python file for learning the basics.

---
+++ integer_float_division.py
@@ -0,0 +1,10 @@
+float_division = 12 / 3 #prints division result with .0
+print (float_division)
+
+integer_division = 12 // 3. #prints division result without .0 (floating point).
+print(integer_division)
+
+integer1_division = 12 // 5  #prints division result without .0 (floating point) & rounds off the number.
+print(integer1_division)
+
+
