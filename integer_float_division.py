dec0f5be57dabeb0e059753296567968d2c8edfb
Rishab Edmund Edmund <98113375+rmeajuri@users.noreply.github.com>
2022-01-20 17:25:45 +0000

    Corrected integer_float_divison.py doc

--- integer_float_division.py
+++ integer_float_division.py
@@ -1,7 +1,7 @@
 float_division = 12 / 3 #prints division result with .0
 print (float_division)
 
-integer_division = 12 // 3. #prints division result without .0 (floating point).
+integer_division = 12 // 3. #prints division result without .0 (floating point). Also Known as Floor Division
 print(integer_division)
 
 integer1_division = 12 // 5  #prints division result without .0 (floating point) & rounds off the number.
