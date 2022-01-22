8f54cb014aba25d8effbcf04135bf4f6c304a518
Rishab Edmund <98113375+rmeajuri@users.noreply.github.com>
2022-01-22 23:18:19 +0000

    boolean operator in python

--- boolean.py
+++ boolean.py
@@ -1,3 +1,4 @@
+print("This is a sample to test the functionality of boolean operators in python as follows: ")
 truth = True
 false = False
 
@@ -12,10 +13,10 @@ print(is_over_age)
 ##### Using boolean operators in a condition check (if/else loop)
 
 my_number = 10
-your_number = int(input("Enter a number: "))
+your_number = int(input("Enter a number, we will check to see if it matches with the records: "))
 
 match_check = my_number == your_number
 if match_check:
   print(f"The number provided by you matches our number: {match_check}")
 else:
-  print("The number entered by you doesn't match our number in the records. Thanks for your time.")
+  print("The number entered by you doesn't match our number in the records. Thanks for your time. Bye..!")
